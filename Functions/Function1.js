0e055d8373e608bda406dab9a6d028760f2a8cf5
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-06 01:49:24 +0530

    Add files via upload

---
+++ Functions/Function1.js
@@ -0,0 +1,63 @@
+ //Functions
+ //Function is a block of code used to
+ //perform a certain task
+ //we need to define a function before using it
+//  function sum()
+//  {
+// var a=10
+// var b=20
+// var sum=a+b
+
+// console.log(sum);
+//  }
+
+//in the below line we are calling function
+//  sum();
+
+
+
+//the below line is an example of 
+//function parameter where in we are
+//sending a b as parameters
+// function sum(a,b){
+// var total=a+b
+// console.log(total);
+// }
+// //the below line is the function argument
+
+// sum(1,2)
+
+
+
+//Function Expressions
+
+
+// function sum(a,b){
+// var total=a+b
+// console.log(total);
+// }
+
+// var funExp=sum(1,2)
+
+// funExp;
+
+// function sum(a,b)
+// {
+//     return a+b
+// }
+
+// var funExp=sum(1,2)
+
+// console.log(funExp);
+
+
+
+//Anonymous function
+
+var anonFun=function(a,b){
+    return a+b
+}
+
+var funExp=anonFun(1,2)
+
+console.log(funExp);
