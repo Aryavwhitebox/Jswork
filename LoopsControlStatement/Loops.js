a1109780b38765623ee5f196594f16203744e59e
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-06 01:54:19 +0530

    Add files via upload

---
+++ LoopsControlStatement/Loops.js
@@ -0,0 +1,130 @@
+// let a=10
+
+// let b=20
+
+// if(a==b){
+//     console.log('equal');
+// }
+
+// else{
+//     console.log('not equal');
+// }
+
+
+//falsy values in javascript
+//there are 5 falsy values
+//0, "", undefined, null, NaN, false 
+
+// if(0){
+//     console.log('true');
+// }
+// else{
+//     console.log('falsy'); //falsy
+// }
+
+
+// if(false){
+//     console.log('true');
+// }
+// else{
+//     console.log('falsy'); //falsy
+// }
+//ternary operator
+
+// let a=18
+
+// console.log((a>18)?'eligible':'not eligible');
+
+
+//ELSE IF LADDER
+
+// var day='monday'
+
+// if(day='monday'){
+//     console.log('we are in monday');
+// }
+
+// else if(day='tuesday'){
+// console.log('tueday');
+// }
+// else if(day='wednesday'){
+//     console.log('wednesday');
+//     }
+//     else if(day='thursday'){
+//         console.log('thursday');
+//         }
+//         else if(day='friday'){
+//             console.log('friday');
+//             }
+// else{
+//     console.log('holiday');
+// }
+
+
+//SWITCH CASE
+
+
+// var day=4
+
+// switch(day){
+//     case 1:console.log('monday');break
+//     case 2:console.log('tuesday');break
+
+//     case 3:console.log('wednesday');break
+//     case 4:console.log('thursday');break
+//     case 5:console.log('friday');break
+//     case 6:console.log('saturday');break
+//     case 7:console.log('sunday');break
+// default:console.log('please enter valid day');
+// }
+
+//WHILE LOOP
+//the while statement creates a loop that executes a
+//specified statement as long as the test condition 
+//evaluates to true
+// var i=0
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+//DO WHILE
+// var i=0
+// do{
+//     i++
+//     console.log(i);
+// }
+
+// while(i<=10)
+
+
+
+
+//FOR LOOP
+// for(var i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+//FOR IN loop
+
+// let students={
+
+//     name:'aryav',
+//     technology:'ui',
+//     role:'instructor'
+
+// }
+// for(let key in students){
+//     console.log(`${key}=>${students[key]}`);
+// }
+
+// let list = [4, 5, 6];
+
+// for (let i in list) {
+//    console.log(i); // "0", "1", "2",
+// }
+
+// for (let i of list) {
+//    console.log(i); // "4", "5", "6"
+// }
+
