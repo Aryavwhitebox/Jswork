48bb03964eeb89c35085facaaf473715399312e2
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-06 01:55:53 +0530

    Add files via upload

---
+++ OperatorsExpression/Operators.js
@@ -0,0 +1,86 @@
+// //comparision operator
+// let x=10
+// let y=10
+// console.log(x===y);
+// //arithmetic
+// let a=5+10
+// console.log(a);
+
+// let a1=10
+
+// if(a1%2==0){
+//     console.log('even');
+// }
+// else{
+//     console.log('odd');
+// }
+
+// //increment and decrement operator
+
+// let a=5
+// let b=a++
+// console.log('post increment');
+// console.log(a,'-',b); //6,5
+
+// let c=3
+// let d=++c
+// console.log('pre increment');
+// console.log(c,'-',d);
+
+// let a=10
+// let b=--a
+// console.log('pre decrement');
+// console.log(a,'-',b);
+
+
+// let c=7
+// let d=c--
+// console.log('post decrement');
+// console.log(c,'-',d);
+
+
+//comparision operator
+
+// var a=10
+// var b=10
+// console.log(a==b);
+// console.log(a>=b);
+
+
+// console.log(a<b);
+
+// console.log(a<=b);
+
+//logical operators
+// let a=20
+// let b=-10
+//&& operator checks and works
+//only when both the conditions are true
+// if(a>b && a>=b){
+//     console.log('a is greater');
+// }
+
+
+//in or operator even when single condition
+//satisfies it will work
+// if(a>b || b>a){
+//     console.log('or operator');
+// }
+
+
+// ! operator
+
+// let a=10
+// let b=20
+// if(a!=b){
+//     console.log('not equal');
+// }
+
+//appende or concator operator
+
+// let a='aryav'
+// let b='ui'
+
+// console.log(a+b); //aryavui
+
+
