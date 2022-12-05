ab9e94052889f80e0ee266cac3e5d72763e4cab9
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-06 01:50:05 +0530

    Add files via upload

---
+++ Interviewques/Interviews.js
@@ -0,0 +1,79 @@
+// console.log('sameer'-'aryav');//NaN
+
+// //the above example is the example of
+// //nan
+
+// let a=123456789
+// console.log(typeof(a));//number
+
+// console.log(isNaN(a));//false
+
+// let b='hello'
+
+// console.log(isNaN(b));//true
+
+// if(isNaN(b)){
+//     console.log('please enter a valid number');
+//     //we can console an error if it is not a number
+// }
+// //CHALLENGE TIME
+
+// console.log(NaN===NaN); //false
+// console.log(Number.NaN===NaN);//false
+
+//what is 3**3
+
+// console.log(3**3);
+//this will take 3 to the exponential of 3
+//which will make 3**3 //3*3*3=27
+
+// console.log(10 + "aryav");
+
+//swapping two numbers without using
+//third variable
+// let a=10
+// let b=5
+
+// a=a+b //15
+
+// b=a-b //15-5 =10
+
+// a=a-b//15-10=5
+
+// console.log(a);
+// console.log(b);
+
+//== and ====
+//== just compares
+//=== compares data type as well
+
+// let a=10
+// let b='10'
+// console.log(a==b); //true
+// console.log(a===b); //false
+
+// console.log(typeof(a)); //number
+// console.log(typeof(b)); //string
+
+//check if the given year is leap year
+
+// let a = 2020;
+
+// if(a%4===0){
+//  if(a%100===0){
+// if(a%400===0){
+//     console.log('leap year');
+// }
+// else{
+//     console.log('not a leap year');
+// }
+//  }
+//  else{
+//     console.log(' leap year');
+//  }
+// }
+
+// else{
+//     console.log('not a leap year');
+// }
+
