b23047a932d87f45dede278e590ef66ae1e0d6bd
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-08 08:18:33 -0800

    Create introthapa.js

---
+++ Intro/introthapa.js
@@ -0,0 +1,77 @@
+// number;
+
+//typeof : return data type of varaible
+// var a=10;
+// console.log(typeof a);
+
+//value type :primitive type
+//reference type : non-primitive type
+
+
+//String
+// var a='10';
+// console.log(typeof a);
+
+// var msg="Hello";
+// console.log(msg);
+
+
+
+//Boolean:
+// var bln=false;
+// console.log(typeof bln);
+
+// var a=null;
+// console.log(typeof a);
+// console.log(a);
+
+
+//Non-primitive data type:
+
+//Object
+//array
+
+// var obj={
+//    myName:'ameer',
+//    number:10,
+//    email:'WBL-learning.com',
+//    x:undefined,
+//    a:null
+// }
+// console.log(typeof obj.number);
+
+
+// //arrays
+// var arr=[1,2,3,5,6];
+// console.log(typeof arr[4]);
+
+
+
+// var a="10.53";
+// var b=20.9999;
+
+// console.log(typeof parseFloat(a));
+
+
+
+
+// diff between null and undefined
+
+
+
+
+// var a=10;
+// var b=20;
+// var sum = a+b;
+// console.log(sum);
+
+
+
+// NaN :not a number
+
+// // isNaN() 
+// var a=20;
+// var b='Hello';
+// console.log(isNaN(a));
+
+// console.log(isNaN(b));
