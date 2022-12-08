ae230ba73fd112f186f8457b624be45a3714e96b
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2022-12-08 08:19:07 -0800

    Create Operatorsthapa.js

---
+++ OperatorsExpression/Operatorsthapa.js
@@ -0,0 +1,173 @@
+// Arithmetic Operators
+// addition(+)
+// subtraction(-)
+// Multiplication(*)
+// Division(/)
+// Exponential power(**)
+// Increment(++)
+// Decrement(--)
+
+const { ADDRCONFIG } = require("dns");
+
+// var a=10, b=20;
+// var sum=a+b;
+// console.log(sum);
+
+// var a='hi', b='Hello';
+// var sum=a+b;
+// console.log(sum); //strings will concat if we use (+) operator
+
+// var a=true, b=true;
+// var sum=a+b;
+// console.log(sum); // in boolean it returns 1+1
+
+// var x=20,y=30,a='10', b=20,c=30;
+// var sum=x+y+a+b+c;
+// console.log(sum);
+
+
+// var a=30, b=20;
+// var sub=a-b;
+// console.log(sub);
+
+
+
+// var a='20', b='10';
+// var sub=a-b;
+// console.log(sub);
+
+// var a=true, b=false;
+// var sub=a-b;
+// console.log(sub);
+
+
+// var a=10, b=20;
+// var mul=a*b;
+// console.log(mul);
+
+
+// var a='hi', b=20;
+// var mul=a*b;
+// console.log(mul);
+
+
+
+// var a='2ga', b=10;
+// var div=a/b;
+// console.log(div); 
+
+
+// exponential 
+// var a=10, b=4;
+// var exp=a**b;
+// console.log(exp);
+
+
+// increment (++)  
+
+// postfix increment (a++) add +1 to variable
+
+// var a=10;
+// console.log(a);// 10
+// console.log(++a); //11
+// console.log(a); //11
+
+
+
+
+// var a=10;
+// console.log(a);// 10
+// console.log(--a); //9
+// console.log(a); //9
+
+
+
+
+//Assignment Operators:
+
+// var a=10; 
+// a/=10;
+// console.log(a);
+
+
+// camparison Operators:
+// < , >,<=,>=,==
+
+// var a=20, b='20';
+// console.log(a===b);  // ===value and data type (false)
+
+
+// var a=10,b=20;
+// console.log(a>=b);
+
+// !=(not equals)
+
+// var a=20,b=20;
+// console.log(a!=b);
+
+
+
+
+
+
+// logical operators
+// AND /OR /NOT
+
+// AND true : if all conditions are true  &&
+// OR True: if any of condition is true   ||
+// NOT (!) : false-true
+
+
+// var a=10 , b=20, c=30;
+// console.log((a>b) || (a>c));
+
+// var a=10, b=20;
+// console.log(!(a>b));
+
+
+
+
+// String Operators
+// + is used concat two string
+
+// var x='Hi',a=' ', y='welcome';
+// console.log(x+a+y);
+
+
+
+// other operators
+// ., new, [], (), ?:
+
+// var obj={
+//     name:'ameer',
+//     number:10
+// }
+// console.log(obj.number);
+
+
+// var arr=[1,2,4,5]
+// console.log(arr[3]);
+
+
+// var str= new String;
+// console.log( typeof str);
+
+
+
+
+// true and falsy
+
+// flasy value: false , 0, undefined, null, NaN and '';
+
+// == & ===
+
+
+
+
+// var a=10, b=20;  //a=20,b=10;
+// var c;
+
+// c=b; 
+// b=a;
+// a=c;
+// console.log(a,b);
