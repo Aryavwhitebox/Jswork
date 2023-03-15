f3243ac7c66b5ff99236dd972a6aff7421e79379
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-03-15 09:31:29 -0700

    Create alertprtconfrm.js

---
+++ 1alertPromptConfirm/alertprtconfrm.js
@@ -0,0 +1,14 @@
+alert('thi')
+
+var a=prompt('this is prompt','123')
+document.write(a)
+
+
+let write=confirm("do you want to write to page")
+if(write){
+
+document.write(a)
+}
+else{
+  document.write("please allow me to write")
+}
