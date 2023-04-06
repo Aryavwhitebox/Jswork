c78ffc61d2d70e5b43737802fbf833c57f011c4b
Aryavwhitebox <97701937+Aryavwhitebox@users.noreply.github.com>
2023-04-06 12:17:08 -0700

    Create Date1.js

---
+++ Date/Date1.js
@@ -0,0 +1,14 @@
+
+var a=new Date();
+fy=a.getFullYear();
+
+console.log(fy)
+mn=a.getMonth();
+console.log(mn)
+console.log("time is")
+hrs=a.getHours();
+
+mnts=a.getMinutes();
+
+sec=a.getSeconds();
+console.log(hrs,mnts,sec)
